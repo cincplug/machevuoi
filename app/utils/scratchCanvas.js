e59d9b1c53f2d3dee1a951f3534c83ccfca7f7d3
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2024-04-04 15:17:07 +0200

    Remove misleading scratch pattern parameter

--- app/utils/scratchCanvas.js
+++ app/utils/scratchCanvas.js
@@ -1,15 +1,7 @@
 import { processColor, getAverageDistance } from "./index";
 
-const getLineWidth = ({
-  minimum,
-  radius,
-  tipDistance,
-  index = 5,
-}) => {
-  return Math.max(
-    minimum,
-    (radius / tipDistance) * index
-  );
+const getLineWidth = ({ minimum, radius, tipDistance, index = 5 }) => {
+  return Math.max(minimum, (radius / tipDistance) * index);
 };
 
 export const scratchCanvas = ({
@@ -19,41 +11,38 @@ export const scratchCanvas = ({
   color,
   opacity,
   tips,
-  scratchPattern,
   lastTips,
-  pinchThreshold,
   pressedKey,
   dispersion,
   lines,
   curves
 }) => {
-  const tipValues = Object.values(tips);
-  const tipDistance = getAverageDistance(tipValues);
   ctx.strokeStyle = processColor(color, opacity);
-  if (lastTips && (pressedKey === "Shift" || tipDistance < pinchThreshold)) {
+  if (lastTips && pressedKey === "Shift") {
     ctx.beginPath();
-    if (scratchPattern === "custom") {
-      if (lines.length > 0) {
-        lines.forEach(({ start, end }, index) => {
-          ctx.lineWidth = getLineWidth({
-            minimum,
-            radius,
-            tipDistance,
-            index,
-            dispersion
-          });
-          ctx.moveTo(start.x, start.y);
-          ctx.lineTo(end.x, end.y);
+    if (lines.length > 0) {
+      lines.forEach(({ start, end }, index) => {
+        ctx.lineWidth = getLineWidth({
+          minimum,
+          radius,
+          tipDistance: radius,
+          index,
+          dispersion
         });
-      }
-      if (curves.length > 0) {
-        curves.forEach(({ start, control, end }) => {
-          ctx.moveTo(start.x, start.y);
-          ctx.quadraticCurveTo(control.x, control.y, end.x, end.y);
-        });
-      }
-      ctx.stroke();
-    } else if (["quadratics", "charts", "joints"].includes(scratchPattern)) {
+        ctx.moveTo(start.x, start.y);
+        ctx.lineTo(end.x, end.y);
+      });
+    }
+    if (curves.length > 0) {
+      curves.forEach(({ start, control, end }) => {
+        ctx.moveTo(start.x, start.y);
+        ctx.quadraticCurveTo(control.x, control.y, end.x, end.y);
+      });
+    }
+    ctx.stroke();
+    if (tips) {
+      const tipValues = Object.values(tips);
+      const tipDistance = getAverageDistance(tipValues);
       tipValues.forEach((_tip, tipIndex) => {
         ctx.lineWidth = getLineWidth({
           tipIndex,
@@ -63,32 +52,22 @@ export const scratchCanvas = ({
           dispersion
         });
         const prevIndex = tipIndex === 0 ? tipValues.length - 1 : tipIndex - 1;
-        if (scratchPattern === "quadratics") {
-          ctx.quadraticCurveTo(
-            tipValues[tipIndex].x,
-            tipValues[tipIndex].y,
-            tipValues[prevIndex].x,
-            tipValues[prevIndex].y
-          );
-        } else if (scratchPattern === "charts") {
-          ctx.rect(
-            tipValues[tipIndex].x,
-            tipValues[tipIndex].y,
-            tipValues[prevIndex].x - tipValues[tipIndex].x,
-            tipValues[prevIndex].y - tipValues[tipIndex].y
-          );
-        } else if (scratchPattern === "joints") {
-          ctx.arc(
-            tipValues[tipIndex].x,
-            tipValues[tipIndex].y,
-            radius,
-            (2 * Math.PI) / tipValues[tipIndex].y,
-            Math.abs(tips[tipIndex].x - tipValues[prevIndex].x)
-          );
-        }
+        // ctx.rect(
+        //   tipValues[tipIndex].x,
+        //   tipValues[tipIndex].y,
+        //   tipValues[prevIndex].x - tipValues[tipIndex].x,
+        //   tipValues[prevIndex].y - tipValues[tipIndex].y
+        // );
+        // ctx.arc(
+        //   tipValues[tipIndex].x,
+        //   tipValues[tipIndex].y,
+        //   tipDistance / radius,
+        //   tipValues[tipIndex].y,
+        //   Math.abs(tips[tipIndex].x - tipValues[prevIndex].x)
+        // );
         ctx.stroke();
       });
-    } else if (["lines", "rectangles"].includes(scratchPattern)) {
+
       Object.keys(tips).forEach((tip, index) => {
         if (!lastTips[tip]) return;
         ctx.moveTo(lastTips[tip].x, lastTips[tip].y);
@@ -99,22 +78,12 @@ export const scratchCanvas = ({
           tipDistance,
           dispersion
         });
-        if (scratchPattern === "lines") {
-          ctx.quadraticCurveTo(
-            lastTips[tip].x,
-            lastTips[tip].y,
-            tips[tip].x,
-            tips[tip].y
-          );
-        }
-        if (scratchPattern === "rectangles") {
-          ctx.rect(
-            lastTips[tip].x,
-            lastTips[tip].y,
-            tips[tip].x - lastTips[tip].x,
-            tips[tip].y - lastTips[tip].y
-          );
-        }
+        ctx.quadraticCurveTo(
+          lastTips[tip].x,
+          lastTips[tip].y,
+          tips[tip].x,
+          tips[tip].y
+        );
         ctx.stroke();
       });
     }
