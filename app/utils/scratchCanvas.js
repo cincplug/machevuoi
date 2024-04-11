d4a4bdea36fcf629d7498c10b2651bea5520a0fc
Luka Stanisavljevic <luka@cincplug.com>
2024-04-11 09:48:53 +0200

    Improve scratch drawing

--- app/utils/scratchCanvas.js
+++ app/utils/scratchCanvas.js
@@ -16,77 +16,67 @@ export const scratchCanvas = ({
   arcs,
   ovals
 }) => {
+  ctx.lineWidth = getLineWidth({
+    minimum,
+    radius,
+    tipDistance: radius,
+  });
   ctx.beginPath();
   if (lines.length > 0) {
     lines.forEach(({ start, end }, index) => {
-      ctx.lineWidth = getLineWidth({
-        minimum,
-        radius,
-        tipDistance: radius,
-        index,
-        dispersion
-      });
       ctx.moveTo(start.x, start.y);
       ctx.lineTo(end.x, end.y);
     });
   }
+
   if (curves.length > 0) {
     curves.forEach(({ start, control, end }) => {
       ctx.moveTo(start.x, start.y);
       ctx.quadraticCurveTo(control.x, control.y, end.x, end.y);
     });
   }
+
   if (arcs.length > 0) {
     arcs.forEach(({ start, end }) => {
       const midX = (start.x + end.x) / 2;
       const midY = (start.y + end.y) / 2;
-  
       const slope = -(start.x - end.x) / (start.y - end.y);
-  
-      const distance = Math.sqrt((start.x - end.x)**2 + (start.y - end.y)**2) / 2;
-  
-      const controlX = midX + distance / Math.sqrt(1 + slope**2);
+      const distance =
+        Math.sqrt((start.x - end.x) ** 2 + (start.y - end.y) ** 2) / 2;
+      const controlX = midX + distance / Math.sqrt(1 + slope ** 2);
       const controlY = midY + slope * (controlX - midX);
-  
       ctx.moveTo(start.x, start.y);
       ctx.quadraticCurveTo(controlX, controlY, end.x, end.y);
     });
   }
-  
+
   if (ovals.length > 0) {
     ovals.forEach(({ start, control, end }) => {
-      const distControl = Math.sqrt((control.x - start.x)**2 + (control.y - start.y)**2);
-      const distEnd = Math.sqrt((end.x - start.x)**2 + (end.y - start.y)**2);
+      const distControl = Math.sqrt(
+        (control.x - start.x) ** 2 + (control.y - start.y) ** 2
+      );
+      const distEnd = Math.sqrt(
+        (end.x - start.x) ** 2 + (end.y - start.y) ** 2
+      );
       const rx = Math.max(distControl, distEnd);
       const ry = Math.min(distControl, distEnd);
-      const rotation = (distControl > distEnd) ? Math.atan2(control.y - start.y, control.x - start.x) : Math.atan2(end.y - start.y, end.x - start.x);
-      
+      const rotation =
+        distControl > distEnd
+          ? Math.atan2(control.y - start.y, control.x - start.x)
+          : Math.atan2(end.y - start.y, end.x - start.x);
+
       const startX = start.x + rx * Math.cos(rotation);
       const startY = start.y + rx * Math.sin(rotation);
-      
+
       ctx.moveTo(startX, startY);
       ctx.ellipse(start.x, start.y, rx, ry, rotation, 0, 2 * Math.PI);
     });
   }
-  
-  
-  
-
   ctx.stroke();
+
   if (tips) {
     const tipValues = Object.values(tips);
     const tipDistance = getAverageDistance(tipValues);
-    tipValues.forEach((_tip, tipIndex) => {
-      ctx.lineWidth = getLineWidth({
-        tipIndex,
-        minimum,
-        radius,
-        tipDistance,
-        dispersion
-      });
-      const prevIndex = tipIndex === 0 ? tipValues.length - 1 : tipIndex - 1;
-      ctx.stroke();
-    });
 
     Object.keys(tips).forEach((tip, index) => {
       if (!lastTips || !lastTips[tip]) return;
