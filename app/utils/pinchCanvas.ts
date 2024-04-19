4b42478b5ecfd844b9c377f1cf797e32329570c3
Luka Stanisavljevic <luka@cincplug.com>
2024-04-19 10:26:14 +0200

    Improve pinch canvas

--- app/utils/pinchCanvas.ts
+++ app/utils/pinchCanvas.ts
@@ -26,26 +26,29 @@ export const pinchCanvas = ({
   lastY,
   activeLayer
 }: PinchCanvasParams): void => {
-  let targetLineWidth = radius - thumbIndexDistance + minimum;
-  ctx.lineWidth = (targetLineWidth * 2 - ctx.lineWidth) / 3;
-
+  ctx.beginPath();
   const shapePainter = shapePainters[activeLayer];
-
   if (shapePainter) {
-    ctx.beginPath();
     if (!lastX) {
       ctx.moveTo(x, y);
     } else {
-      if (activeLayer === "dots") {
-        ctx.moveTo(lastX, lastY);
-        ctx.quadraticCurveTo(lastX, lastY, x, y);
+      if (dispersion > 0) {
+        const distance = getDistance({ x, y }, { x: lastX, y: lastY });
+        ctx.lineWidth = Math.max(minimum, radius / distance) + dispersion;
       }
-      const distance = getDistance({ x, y }, { x: lastX, y: lastY });
-
-      if (distance > dispersion) {
-        shapePainter({ ctx, start: { x: lastX, y: lastY }, end: { x, y }, control: {x: (lastX + x) / 2, y: (lastY + y) / 2} });
-      }
+      shapePainter({
+        ctx,
+        start: { x: lastX, y: lastY },
+        end: { x, y },
+        control: { x: (lastX + x) / 2, y: (lastY + y) / 2 }
+      });
     }
+  } else {
+    let targetLineWidth = Math.max(minimum, radius / thumbIndexDistance);
+    ctx.lineWidth =
+      (targetLineWidth * dispersion - ctx.lineWidth) / (dispersion + 1);
+    ctx.moveTo(lastX, lastY);
+    ctx.quadraticCurveTo(lastX, lastY, x, y);
   }
   ctx.stroke();
   lastX = x;
