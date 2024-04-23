9fd7f5cdb194ecbbf2bf4497cb23353c1ad8784b
Luka Stanisavljevic <luka@cincplug.com>
2024-04-23 22:27:29 +0200

    Convert Dots to TS

--- app/components/shapes/Dots.tsx
+++ app/components/shapes/Dots.tsx
@@ -1,7 +1,11 @@
-import React from "react";
 import HP from "../../data/handPoints.json";
 
-function Dots({ selectedDots, handleDotClick }) {
+interface DotsProps {
+  selectedDots: number[];
+  handleDotClick: (index: number) => void;
+}
+
+const Dots: React.FC<DotsProps> = ({ selectedDots, handleDotClick }) => {
   return HP.map((point, index) => (
     <circle
       key={index}
