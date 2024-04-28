577996c13cd7ab7eb02cb871cd4a6af9458382ea
Luka Stanisavljevic <luka@cincplug.com>
2024-04-28 10:05:27 +0200

    Improve slogan

--- app/components/nav/Splash.tsx
+++ app/components/nav/Splash.tsx
@@ -7,12 +7,12 @@ interface IProps {
 const Splash: React.FC<IProps> = ({ handlePlayButtonClick }) => {
   return (
     <div className="wrap splash">
-      <h1>
-        <span className="text">Ma che vuoi?</span>
-      </h1>
-      <div className="slogan">
-        <div className="slogan-text">
-          <p>Paint with your hand in the thin air</p>
+      <div>
+        <h1>
+          <span className="text">Ma che vuoi?</span>
+        </h1>
+        <div className="slogan">
+          <p className="slogan-text">Paint with bare hand<br/>in the thin air</p>
         </div>
       </div>
       <button className="splash-button" onClick={handlePlayButtonClick}>
