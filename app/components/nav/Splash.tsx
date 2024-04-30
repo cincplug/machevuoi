67393a1baf437435f4bfe917de44a21e1e6f9a3b
Luka Stanisavljevic <luka@cincplug.com>
2024-04-30 16:34:23 +0200

    Adjust initial transition

--- app/components/nav/Splash.tsx
+++ app/components/nav/Splash.tsx
@@ -9,13 +9,11 @@ const Splash: React.FC<IProps> = ({ handlePlayButtonClick }) => {
     <div className="wrap splash">
       <section>
         <h1>Ma che vuoi?</h1>
-        <div className="slogan">
-          <p className="slogan-text">
-            Paint with your bare hand
-            <br />
-            in the thin air
-          </p>
-        </div>
+        <p className="slogan">
+          Paint with your bare hand
+          <br />
+          in the thin air
+        </p>
       </section>
       <button className="splash-button" onClick={handlePlayButtonClick}>
         Start camera
