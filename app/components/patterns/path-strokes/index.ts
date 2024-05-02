ecce994e9ab8646ae979eced283d45f3dca8ac0b
Luka Stanisavljevic <luka@cincplug.com>
2024-05-02 17:29:15 +0200

    Add some svg scenarios

--- app/components/patterns/path-strokes/index.ts
+++ app/components/patterns/path-strokes/index.ts
@@ -1,16 +1,10 @@
 import { IPoint } from "../../../../types";
 
-type StrokeType =
-  | "lines"
-  | "cloud"
-  | "blast"
-  | "beziers"
-  | "peas";
+type StrokeType = "lines" | "cloud" | "curls" | "beziers" | "cable";
 
 interface PathStrokeProps {
   pathStroke: StrokeType;
   thisPoint: IPoint;
-  controlPoint: IPoint;
   nextPoint: IPoint;
   radius: number;
 }
@@ -18,22 +12,24 @@ interface PathStrokeProps {
 const pathStrokes = ({
   pathStroke,
   thisPoint,
-  controlPoint,
   nextPoint,
-  radius,
+  radius
 }: PathStrokeProps) => {
   const strokeDefinitions = {
     beziers: `Q ${thisPoint.x} ${thisPoint.y}, ${
       (thisPoint.x + nextPoint.x) / 2
     } ${(thisPoint.y + nextPoint.y) / 2}`,
     lines: `L${thisPoint.x} ${thisPoint.y} `,
-    cloud: `A${radius} ${radius} 0 0 1 ${thisPoint.x} ${
-      thisPoint.y
-    }`,
-    blast: `Q ${thisPoint.x} ${thisPoint.y} ${controlPoint.x} ${controlPoint.y}`,
-    peas: `C ${nextPoint.x} ${nextPoint.y}, ${nextPoint.x} ${
-      nextPoint.y
-    }, ${(nextPoint.x + thisPoint.x) / 2} ${(nextPoint.y + thisPoint.y) / 2}`
+    cloud: `A${radius} ${radius} 0 0 1 ${thisPoint.x} ${thisPoint.y}`,
+    curls: `A14,14 0 0 1 ${nextPoint.x},${nextPoint.y}
+    A27,27 0 0 1 ${nextPoint.x},${nextPoint.y}
+    A22,22 0 0 1 ${thisPoint.x},${thisPoint.y}
+    A31,31 0 0 1 ${thisPoint.x},${thisPoint.y}`,
+    cable: `C${thisPoint.x + radius * radius},${
+      thisPoint.y + radius * radius
+    } ${nextPoint.x - radius * radius},${nextPoint.y - radius * radius} ${
+      nextPoint.x
+    },${nextPoint.y}`
   };
   return strokeDefinitions[pathStroke];
 };
