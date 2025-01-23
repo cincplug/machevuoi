4dac28cc83750c90f808f529bd492fdf4166d61b
Luka Stanisavljevic <luka@cincplug.com>
2025-01-23 16:26:42 +0100

    Improve bitmap manipulation in selection

--- app/components/shapes/Bitmap.tsx
+++ app/components/shapes/Bitmap.tsx
@@ -1,5 +1,6 @@
 import { getPoint } from "../../utils";
 import { IShape } from "../../../types";
+import { useState, useEffect } from "react";
 
 interface BitmapProps {
   shape: IShape;
@@ -14,26 +15,49 @@ const Bitmap: React.FC<BitmapProps> = ({
   onClick,
   isPreview = false
 }) => {
+  const [imageRatio, setImageRatio] = useState(1);
   const { x: spx, y: spy } = getPoint(startPoint, isPreview);
   const { x: epx, y: epy } = getPoint(endPoint, isPreview);
 
   const height = Math.hypot(epx - spx, epy - spy);
-  const width = height * (16 / 9);
+  const width = height * imageRatio;
   const angle = Math.atan2(epy - spy, epx - spx) - Math.PI / 2;
 
+  const transform = `rotate(${(angle * 180) / Math.PI} ${spx} ${spy})`;
+
+  useEffect(() => {
+    const img = new Image();
+    img.onload = () => {
+      setImageRatio(img.width / img.height);
+    };
+    img.src = "/image.png";
+  }, []);
+
   return (
-    <image
-      href="/image.png"
-      x={spx - width / 2}
-      y={spy}
-      width={width}
-      height={height}
-      className={`scratch-path ${isPreview ? "preview" : ""}`}
-      onClick={onClick}
-      transform={`rotate(${(angle * 180) / Math.PI} ${spx} ${spy})`}
-    >
-      <title>{title}</title>
-    </image>
+    <g transform={transform}>
+      <image
+        href="/image.png"
+        x={spx - width / 2}
+        y={spy}
+        width={width}
+        height={height}
+        className={`scratch-path ${isPreview ? "preview" : ""}`}
+      >
+        <title>{title}</title>
+      </image>
+      <polyline
+        points={`
+          ${spx - width / 2},${spy}
+          ${spx + width / 2},${spy}
+          ${spx + width / 2},${spy + height}
+          ${spx - width / 2},${spy + height}
+          ${spx - width / 2},${spy}
+        `}
+        fill="none"
+        className={`scratch-path bitmap-outline ${isPreview ? "preview" : ""}`}
+        onClick={onClick}
+      />
+    </g>
   );
 };
 
